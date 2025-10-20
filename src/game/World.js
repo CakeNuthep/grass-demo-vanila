ae0ea7882aacdd1b8b262cd4721643abe662e0e4
Suppachai Nuthep <cake.nuthep@gmail.com>
2025-10-20 21:24:30 +0700

    edit path

--- src/game/World.js
+++ src/game/World.js
@@ -11,9 +11,9 @@ export default class World {
 		this.game = new Game();
 		this.scene = this.game.scene;
 		this.assets = {
-			level: "/src/assets/models/level.glb",
-			heightMap: "/src/assets/textures/heightMap.png",
-			grassTexture: "/src/assets/textures/grass.jpg",
+			level: "./src/assets/models/level.glb",
+			heightMap: "./src/assets/textures/heightMap.png",
+			grassTexture: "./src/assets/textures/grass.jpg",
 		};
 
 		this.updatables = [];
